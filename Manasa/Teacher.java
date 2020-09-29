5488eb455834bdad6167681fb6fb5fa751856a29
Manasa-Raj <manasar.xworkz@gmail.com>
2020-09-29 13:01:16 +0530

    My first commit

---
+++ Manasa/Teacher.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheritance;
+
+public class Teacher {
+	String schoolName;
+	String designation;
+	public void duty() {
+		System.out.println("Teaching.. ");
+	}
+	public void givingTests() {
+		System.out.println("Giving Tests..");
+	}
+
+}
